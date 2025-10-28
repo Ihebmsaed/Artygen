46ef9fa6ee66bc88aacf23ca162032b28bbf9d9f
Ihebmsaed <ihebmsaed41@gmail.com>
2025-10-28 15:30:35 +0100

    supp

--- Artygen/config_secrets_example.py
+++
@@ -1,5 +0,0 @@
-# Configuration des clés API - NE PAS COMMIT CE FICHIER
-# Copiez ce fichier vers secrets.py et ajoutez vos vraies clés
-
-GEMINI_API_KEY = "AIzaSyDRyS1gBkkiauaY75GvzGdu0iubNzwXjm0"
-HUGGINGFACE_TOKEN = "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"
